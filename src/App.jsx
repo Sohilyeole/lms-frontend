a6eda85a7b35c1e3715da3352077375d35081166
Sohil yeole <yeolesohil44@gmail.com>
2023-10-17 23:41:10 +0530

    adding homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Routes } from "react-router-dom";
 import "./App.css";
+import { Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage"
 import Footer from "./Components/Footer";
+import HomeLayout from "./Layouts/HomeLayout";
 
 function App() {
   return (
     <>
-      {/*<Routes>
-        <Route path="" element={<home/>}></Route>
-  </Routes>*/}
-  <Footer/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+  </Routes>
+ 
     </>
   );
 }
