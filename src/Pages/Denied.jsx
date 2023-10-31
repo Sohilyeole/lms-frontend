96db856187455693f67a284af218528803c79972
Sohil yeole <yeolesohil44@gmail.com>
2023-10-31 18:46:46 +0530

    added denied page

---
+++ src/Pages/Denied.jsx
@@ -0,0 +1,20 @@
+import {  useNavigate } from "react-router-dom"
+
+function Denied(){
+    const navigate=useNavigate();
+return(
+    <main className=" h-screen flex flex-col justify-center items-center bg-[#1a2238]">
+      <h1 className="text-9xl font-extrabold text-white tracking-widest">
+        4O3
+      </h1>
+      <div className="bg-black text-white  px-2 text-sm rounded rotate-12 absolute">
+        Acess denied
+      </div>
+      <button onClick={()=>navigate(-1)} className="mt-5">
+       
+       <span className="relative block px-8 py-3 bg-[#1a2238] border border-current">Go Back</span>
+      </button>
+    </main>
+)
+}
+export default Denied
